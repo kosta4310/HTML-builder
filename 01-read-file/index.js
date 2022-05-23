c8ed1d0629f651eaba2ac77c30e6cf3b17ad772a
kosta4310 <realanka77@gmail.com>
2022-05-23 14:35:04 +0300

    fix: add errors handler

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,3 +5,6 @@ const input = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 input.on('data', (data) => {
     process.stdout.write(data);
 })
+input.on('error', (err) => {
+    console.log(`произошла ошибка ${err}`);
+})
